bd204d1248392cdb6d410194039bd214a660b32b
Jarmo Pertman <jarmo.p@gmail.com>
2013-01-10 22:41:15 +0200

    Change guards for phantomjs.

--- filefield_spec.rb
+++ filefield_spec.rb
@@ -110,18 +110,20 @@ describe "FileField" do
 
   describe "#set" do
     not_compliant_on [:webdriver, :iphone] do
-      it "is able to set a file path in the field and click the upload button and fire the onchange event" do
-        browser.goto WatirSpec.url_for("forms_with_input_elements.html", :needs_server => true)
+      bug "https://github.com/detro/ghostdriver/issues/158", :phantomjs do 
+        it "is able to set a file path in the field and click the upload button and fire the onchange event" do
+          browser.goto WatirSpec.url_for("forms_with_input_elements.html", :needs_server => true)
 
-        path    = File.expand_path(__FILE__)
-        element = browser.file_field(:name, "new_user_portrait")
+          path    = File.expand_path(__FILE__)
+          element = browser.file_field(:name, "new_user_portrait")
 
-        element.set path
+          element.set path
 
-        element.value.should include(File.basename(path)) # only some browser will return the full path
-        messages.first.should include(File.basename(path))
+          element.value.should include(File.basename(path)) # only some browser will return the full path
+          messages.first.should include(File.basename(path))
 
-        browser.button(:name, "new_user_submit").click
+          browser.button(:name, "new_user_submit").click
+        end
       end
 
       it "raises an error if the file does not exist" do
@@ -135,35 +137,35 @@ describe "FileField" do
 
   describe "#value=" do
     not_compliant_on [:webdriver, :iphone] do
-      it "is able to set a file path in the field and click the upload button and fire the onchange event" do
-        browser.goto WatirSpec.url_for("forms_with_input_elements.html", :needs_server => true)
+      bug "https://github.com/detro/ghostdriver/issues/158", :phantomjs do
+        it "is able to set a file path in the field and click the upload button and fire the onchange event" do
+          browser.goto WatirSpec.url_for("forms_with_input_elements.html", :needs_server => true)
 
-        path    = File.expand_path(__FILE__)
-        element = browser.file_field(:name, "new_user_portrait")
+          path    = File.expand_path(__FILE__)
+          element = browser.file_field(:name, "new_user_portrait")
 
-        #element.value = path
-        element.value.should include(File.basename(path)) # only some browser will return the full path
+          element.value = path
+          element.value.should include(File.basename(path)) # only some browser will return the full path
+        end
       end
     end
 
     not_compliant_on :ie, [:webdriver, :chrome], [:webdriver, :iphone] do
-      bug "https://code.google.com/p/phantomjs/issues/detail?id=941", [:webdriver, :phantomjs] do
-        # for chrome, the check also happens in the driver
-        it "does not raise an error if the file does not exist" do
-          path = File.join(Dir.tmpdir, 'unlikely-to-exist')
-          browser.file_field.value = path
+      # for chrome, the check also happens in the driver
+      it "does not raise an error if the file does not exist" do
+        path = File.join(Dir.tmpdir, 'unlikely-to-exist')
+        browser.file_field.value = path
 
-          expected = path
-          expected.gsub!("/", "\\") if WatirSpec.platform == :windows
+        expected = path
+        expected.gsub!("/", "\\") if WatirSpec.platform == :windows
 
-          browser.file_field.value.should == expected
-        end
+        browser.file_field.value.should == expected
+      end
 
-        it "does not alter its argument" do
-          value = '/foo/bar'
-          browser.file_field.value = value
-          value.should == '/foo/bar'
-        end
+      it "does not alter its argument" do
+        value = '/foo/bar'
+        browser.file_field.value = value
+        value.should == '/foo/bar'
       end
     end
   end
